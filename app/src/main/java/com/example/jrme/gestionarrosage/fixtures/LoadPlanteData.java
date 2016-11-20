f177985e549aaa75c85e452ac6391f5d7afb7b54
Jerome Verlyck <verlyck.jerome@gmail.com>
2016-11-20 17:54:48 +0100

    Ajout de fixtures pour la table Plante

---
+++ app/src/main/java/com/example/jrme/gestionarrosage/fixtures/LoadPlanteData.java
@@ -0,0 +1,41 @@
+package com.example.jrme.gestionarrosage.fixtures;
+
+import com.example.jrme.gestionarrosage.PlanteDatabase;
+
+/**
+ * Class LoadPlanteData
+ * Author Jérôme Verlyck
+ */
+public class LoadPlanteData {
+
+    /**
+     * Méthode qui stock des données de test dans la base de données
+     */
+    public void load() {
+
+        PlanteDatabase planteDatabase = new PlanteDatabase(null);
+
+        String[][] plantes = {
+            { "Acacia", "5", "Jardin Gauche"},
+            { "Azalée", "3", "Jardin Gauche"},
+            { "Bleuet", "10", "Jardin Droite"},
+            { "Camélia", "1", "Jardin Droite"},
+            { "Capucine", "5", "Jardin Bord"},
+            { "Coquelicot", "8", "Jardin"},
+            { "Phalaenopsis mauve", "7", "Séjour"},
+            { "Anthurium", "1", "Toilette"},
+            { "Guzmania", "5", "Couloir"},
+            { "Hibiscus jaune", "15", "Cuisine"},
+            { "Hoya australis", "4", "Salle de bain"},
+            { "Vanda", "1", "Salle à manger"},
+        };
+
+        for(int i = 0; i < plantes.length; i++) {
+            planteDatabase.add(
+                plantes[i][0],
+                Integer.parseInt(plantes[i][1]),
+                plantes[i][2]
+            );
+        }
+    }
+}
